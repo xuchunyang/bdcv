ae92d62b8c4d1045fdf82ec2c1178dafcfb7c854
Xu Chunyang <mail@xuchunyang.me>
2018-11-05 18:14:21 +0800

    Build request URL

--- bing-dict.js
+++ bing-dict.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-console.log('Hello, World')
+const querystring = require('querystring');
+
+const baseUrl = "http://www.bing.com/dict/search?mkt=zh-cn&q=";
+const word = 'hello world';
+const url = baseUrl + querystring.escape(word);
+
+console.log(`TODO: Fetching ${url}...`);
